347c0ca7a8d71da6ff48538b4427303cc331d8ff
Bjorn Augestad <103145@norwegian.com>
2023-04-25 16:30:28 +0200

    Intermediate commit: lines showing (vbf)
    We're able to display the first n lines. They're not folded though, so we miss
    a lot of information. No worries, we can fix that :)

--- vbf.c
+++ vbf.c
@@ -32,6 +32,10 @@
 
 #include <ncurses.h>
 
+void *g_mem;
+size_t g_memsize;
+const char *g_cmem;
+
 __attribute__((format(printf,1,2)))
 static void die(const char *fmt, ...)
 {
@@ -82,22 +86,168 @@ static void mmap_file(const char *filename, void **mem, size_t *memsize)
         die("vbf: file %s is empty\n", filename);
 
     *memsize = st.st_size;
-    *mem = mmap(NULL, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
+    *mem = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
     if (*mem == MAP_FAILED)
         die_perror("%s", filename);
 
     close(fd);
 }
 
+// Do we want to index the mmapped file? We do need to
+// know the start and end of each line, so we later can
+// display those lines. Since each line ends at \n and
+// next line always starts at \n + 1, we don't need to
+// index both the start and end of a line.
+//
+// What if the file is binary without any \n? I guess
+// that makes it a one-liner?
+//
+// What if the last line is missing a newline? Then the
+// end is at memsize. Same goes for single-line files.
+//
+// How do we store the indices? In a long array where
+// the array index == line number? Makes sense, but
+// we will need lots and lots of those indices. 
+// The production logs are big, 33,526,328 lines for
+// today's first 12 hours of production. IOW, we must
+// scale for hundreds of millions of lines. One size_t
+// is 8 bytes. 100 million line indices will therefore
+// use 800MB of RAM. That's a lot, and kinda the opposite 
+// of what we're trying to achieve here.
+//
+// Can we do partial indexing? I guess we can, but how?
+// Can we use datatypes smaller than size_t? Not really.
+// What if we prealloc and later have to realloc()? Can't see a problem RN :)
+// Can we assume that the data is static? Yes, if we MAP_PRIVATE.
+//
+// Let's give indexing a go and time it.
+// And here are the results: It takes 1:24m to index 17GB (33" lines)
+// when caches are dropped prior to indexing. That's of course way
+// too long. Vim (view -n) uses 1:50m, for comparison. 
+//
+// Anyway, we still need to know the start and end of the
+// lines we want to display. We can't wait for minutes
+// so we need to index on demand. How?
+// 1. We always start at the top of the file (do we really?),
+//    so for starters we can index nrows lines.
+
+// 2. When the user scrolls down, we index the lines about to be displayed.
+//    We may want to avoid reindexing if the user scrolls down, then up, then down.
+//    Probably not a biggie, if indexing 200 lines is quick. Let's measure :)
+//    a) Indexing 1000 lines takes 0.8 seconds
+//    b) Indexing 200 lines takes 0.8 seconds
+
+//    Not sure if these numbers are correct as e.g. 1" takes 1.4 seconds.
+//    Why the non-linear performance? Probably due to IO.
+
+//    Anyway, we still need a sliding window and dynamic indexing, so let's
+//    just do that. How? It gets very tricky if we implement the 
+//    commands g and G for top and bottom. Going to the bottom will
+//    either require a lot of indexing, or we have to be clever and index
+//    from the end and backwards. We can do that.
+
+// 3. If we implement "goto line" then the user may choose to go to
+//    the middle of the file. That requires a lot of indexing too.
+//    
+// Should we use an indexing thread? Not sure. Let's start without one.
+// Let's see how far we can go using IOD (indexing on demand).
+
+#if 0
+#define NROWS_INITIAL (1024*1024*100) // 100 MB rows
+#else
+#define NROWS_INITIAL 1000000
+#endif
+
+static size_t *prows;    // The pointer to all our rows
+static size_t nrowsused; // Total number of used elements in the prows array
+static size_t rowsize;   // Total number of allocate elements
+
+static void index_init(void)
+{
+    size_t cb = sizeof *prows * NROWS_INITIAL;
+
+    if (prows == NULL && (prows = malloc(cb)) == NULL)
+        die("Out of memory");
+
+    memset(prows, 0, cb);
+    nrowsused = 0;
+    rowsize = NROWS_INITIAL;
+}
+
+static inline int index_full(void)
+{
+    return nrowsused == rowsize;
+}
+
+
+// Double the size of the index, but not exponentially.
+// We add NROWS_INITIAL each time the index goes full.
+// If that's too slow, go exponential.
+static void index_expand(void)
+{
+    size_t newsize = rowsize + NROWS_INITIAL;
+    void *tmp;
+
+    tmp = realloc(prows, newsize);
+    if (tmp == NULL)
+        die("Out of memory");
+    
+}
+
+static void index_add(size_t val)
+{
+    if (index_full())
+        index_expand();
+
+    prows[nrowsused++] = val;
+}
+
+static void index_file(const void *mem, size_t memsize, size_t nlines)
+{
+    const char *s, *cmem = mem;
+
+    (void)memsize;
+    index_init();
+
+    // First index is always at 0, i.e. start of memory
+    index_add(0);
+
+    while (nlines-- && (s = strchr(cmem, '\n')) != NULL) {
+        assert(*s == '\n');
+        ptrdiff_t delta = s - (const char*)mem + 1; // Offset from start of file. Skip newline
+        index_add((size_t)delta);
+        cmem = s + 1;
+    }
+
+    // TODO: Last line issues? What if last char ain't newline?
+}
+
+static size_t get_row(size_t index, int *rowlen)
+{
+    assert(index < nrowsused);
+
+    *rowlen = prows[index + 1] - prows[index]; // -1 for newline, which we skip
+    return prows[index];
+}
+
+static void dump(const char *s, size_t n)
+{
+    FILE *fp = fopen("dump.txt", "a");
+    while (n--)
+        fputc(*s++, fp);
+
+    fclose(fp);
+}
+
 int main(int argc, char *argv[])
 {
-    void *mem;
-    size_t memsize;
     const char *filename = argv[1];
     int nrows, ncols;
 
     check_args(argc, argv);
-    mmap_file(filename, &mem, &memsize);
+    mmap_file(filename, &g_mem, &g_memsize);
+    g_cmem = g_mem;
+
 
     // Now ncurses initialization
     initscr();
@@ -105,20 +255,40 @@ int main(int argc, char *argv[])
     noecho();
     getmaxyx(stdscr, nrows, ncols);
 
+    if (1) index_file(g_mem, g_memsize, nrows * 2);
+
+    // Now show the first nrows of the file, just to see that we're not fscked
+    if (1) {
+        int i;
+
+        for (i = 0; i < nrows; i++) {
+            int len;
+            size_t offset = get_row(i, &len);
+            if (len == 0)
+                continue; // blank line
+
+            mvaddnstr(i, 0, &g_cmem[offset], len);
+            dump(&g_cmem[offset], len);
+        }
+
+        refresh();
+
+        getch();
+        goto teardown; // Early exit for profiling reasons
+    }
+
+
     // Put some text on the window 
     mvprintw(1, 10, "at y==1, x==10. Screen size is %d rows and %d cols", nrows, ncols);
     mvprintw(10, 1, "press any key to exit");
+    mvaddnstr(0, 0, "hello, world", 10);
     refresh();
     getch();
 
-    // Do we want to index the mmapped file? We do need to
-    // know the start and end of each line, so we later can
-    // display those lines. 
 
-
-    // Teardown
+ teardown:
     endwin(); 
-    munmap(mem, memsize);
+    munmap(g_mem, g_memsize);
     return 0;
 }
 
